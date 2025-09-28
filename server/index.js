d9aaa7eaa5a4fb592aa84e6590f3b6a3134fd4c6
Navnak <navnit.23bce11827@vitbhopal.ac.in>
2025-09-28 22:47:39 +0530

    fix cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const todoRoutes = require("./routes/todo.routes");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://my-todo-five-lilac.vercel.app']
 
 app.use(cors({
     origin: function (origin, callback) {
